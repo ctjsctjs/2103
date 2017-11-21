0b3c1d658918d23e557e0b2aa30e9c3fabf9b5c8
jeremyteh <magnificentbluez@hotmail.com>
2017-11-21 16:00:12 +0800

    WIP

--- foodfinderapp/phpAdminAccountActivationMailer.php
+++ foodfinderapp/phpAdminAccountActivationMailer.php
@@ -4,10 +4,11 @@ require("PHPMailer_5.2.4/class.phpmailer.php");
 
 $mail = new PHPMailer();
 $mail->IsSMTP(); // set mailer to use SMTP
-$mail->SMTPDebug  = 0;
+//$mail->SMTPDebug  = 0;
 //$mail->Debugoutput = 'html';
 //$mail->From = "quizgroup24@gmail.com";
-$mail->FromName = "Food Finder App";
+$mail->From = "jeremyteh8@gmail.com";
+$mail->FromName = "Foodpark";
 $mail->Host = "smtp.gmail.com"; // specif smtp server
 $mail->SMTPSecure= "ssl"; // Used instead of TLS when only POP mail is selected
 $mail->Port = 465; // 465 Used instead of 587 when only POP mail is selected
